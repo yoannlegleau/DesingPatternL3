9d16347bf51f05c4c379cfbd6f435f6f3e7b5680
yoann LE GLEAU <81438255+yoannlegleau@users.noreply.github.com>
2023-03-15 14:39:09 +0100

    corection des warning

---
+++ src/systhem_fichier/troisieme_partie/BaseDonnees.java
@@ -0,0 +1,67 @@
+/**
+ * @author LE GLEAU Yoann
+ * @version 1, 07/03/2023
+ */
+
+package systhem_fichier.troisieme_partie;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Une base de données contient une liste de chaînes de caractères allant de 0 à 100
+ */
+public class BaseDonnees {
+
+    BaseDonnees instance;
+
+    /**
+     * Stratégie de tri à utiliser
+     */
+    StrategieTri strategieTri;
+
+    /**
+     * Liste de données sous forme de chaînes de caractères "Donnée 0", "Donnée 1", ...
+     */
+    List<String> donnees;
+
+
+    public BaseDonnees getInstance(){
+        if (instance == null)
+            instance = new BaseDonnees();
+        return instance;
+    }
+
+    /**
+     * Constructeur de la base de données
+     */
+    private BaseDonnees() {
+        this.strategieTri = new TriJava(); //Tri par default
+        donnees = new LinkedList<>();
+
+        creerDonnees();
+    }
+
+    public void setStrategieTri(StrategieTri strategieTri) {
+        this.strategieTri = strategieTri;
+    }
+
+    /**
+     * Crée une liste de données sous forme de chaînes de caractères "Donnée 0", "Donnée 1", ... jusqu'à 100
+     */
+    private void creerDonnees() {
+        for (int i = 0; i < 100; i++)
+            donnees.add("Donnée " + i);
+        Collections.shuffle(donnees);
+    }
+
+    /**
+     * Affiche les données triées selon la stratégie de tri
+     */
+    public void afficheDonneesTriees() {
+        for (String donnee : strategieTri.trie(donnees))
+            System.out.println(donnee);
+    }
+
+}
---
+++ src/systhem_fichier/troisieme_partie/Test.java
@@ -0,0 +1,27 @@
+/**
+ * @author LE GLEAU Yoann
+ * @version 1, 07/03/2023
+ */
+
+package systhem_fichier.troisieme_partie;
+
+/**
+ * Classe Test qui permet de tester les différents tris et stratégies
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("\nTest TriJava");
+        BaseDonnees maBase = new BaseDonnees(new TriJava());
+        maBase.afficheDonneesTriees();
+
+        System.out.println("\nTest TriBulles");
+        BaseDonnees maBase2 = new BaseDonnees(new TriBulles());
+        maBase2.afficheDonneesTriees();
+
+        System.out.println("\nTest TriSelection");
+        BaseDonnees maBase3 = new BaseDonnees(new TriSelection());
+        maBase3.afficheDonneesTriees();
+    }
+
+}
